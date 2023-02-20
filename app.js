b1ccda70b10371895f602ef03942acffffafa61e
Sayemhaque <mdsayemofficial1000@gmail.com>
2023-02-19 17:03:45 -0800

    env file added

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-const express = require("express");
-const mongoose = require("mongoose")
-const app = express();
-const PORT = 3000;
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotevn from "dotenv";
+
+dotevn.config();
+
+const app = express()
+const PORT = process.env.PORT || 4001;;
+
+
+// CONFIGARATION
+ app.use(bodyParser.json({limit:"30mb" , extended:true}));
+ app.use(bodyParser.urlencoded({limit:"30mb" , extended:true}));
+ app.use(cors())
+
+
 
 // database connection
-const uri =
-  "mongodb+srv://mdsayem999:P1IVmTvmPYoNE6dG@cluster0.rcwosvx.mongodb.net/?retryWrites=true&w=majority";
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(uri, {
+.connect(process.env.URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -21,12 +33,15 @@ mongoose
     }
   );
 
-
 app.get("/api", (req, res) => {
   res.send({ id: 1, name: "Md sayem", address: "kandipara Brahmanbaria" });
 });
 
 // run the server
 app.listen(PORT, () => {
+ try{
   console.log(`server is running on http://localhost:${PORT}/`);
+ }catch(error){
+  console.log(error)
+ }
 });
